b6d2e560af3cbd329b5b4f946b296bb0a874a3cb
Sukhyun Baik <neonagx@gmail.com>
2016-11-30 12:11:02 -0800

    adds strike through

--- js/toDosController.js
+++ js/toDosController.js
@@ -7,9 +7,9 @@
     $scope.add = addTodo
     $scope.newTodo = {name: "", task: "", image: "", complete: false }
     $scope.all = [
-      {name: "Groceries", task: "buy bananas", image: "http://www.allaccessdisney.com/assets/groceries1.jpg", complete: false},
+      {name: "Groceries", task: "buy bananas", image: "http://www.allaccessdisney.com/assets/groceries1.jpg", complete: true},
       {name: "feed cat", task: "put food in bowl", image: "http://www.pfma.org.uk/_assets/images/general/image/Cat%20Drinking%20from%20Bowl%20wb.jpg", complete: true},
-      {name: "Clean Bathroom", task: "scrub shower", image:"http://s3-production.bobvila.com/articles/wp-content/uploads/2013/07/shower-head.jpg" , complete:false},
+      {name: "Clean Bathroom", task: "scrub shower", image:"http://s3-production.bobvila.com/articles/wp-content/uploads/2013/07/shower-head.jpg" , complete:true},
     ]
     function addTodo(){
       $scope.all.push($scope.newTodo)
